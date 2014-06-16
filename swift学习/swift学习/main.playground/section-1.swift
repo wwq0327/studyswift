1b5e2f606431e0faf45018d06beed12737d73148
wwq0327 <wwq0327@gmail.com>
2014-06-17 07:39:56 +0800

    f

--- swift学习/swift学习/main.playground/section-1.swift
+++ swift学习/swift学习/main.playground/section-1.swift
@@ -42,3 +42,45 @@ for i in 0...3 {  // 这一点很像ruby的语法
     firstLoop += i
 }
 firstLoop
+
+// 函数作为返回值
+func returnFifteen() -> Int
+{
+    var y = 10
+    func add() {
+        y += 5
+    }
+    add()
+    return y
+}
+returnFifteen()
+
+// 函数作为别一个函数的返回值
+func makeIncrementer() -> (Int -> Int) {
+    func addOne(number: Int) -> Int {
+        return 1 + number
+    }
+    return addOne
+}
+
+var increment = makeIncrementer()
+increment(7)
+
+// 函数作为传入参数
+func hasAnyMatches(list: Int[], condition: Int -> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    return false
+}
+
+func lessThanTen(number: Int) -> Bool {
+    return number < 10
+}
+
+var numbers = [4, 5, 12, 14, 43,32,2]
+hasAnyMatches(numbers, lessThanTen)
+
+// 匿名函数
