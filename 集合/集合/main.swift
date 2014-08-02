bc5546d92fd0757ee5911f19569ebcadb8fc60f3
wwq0327 <wwq0327@gmail.com>
2014-08-02 22:27:50 +0800

    f

--- 集合/集合/main.swift
+++ 集合/集合/main.swift
@@ -8,5 +8,25 @@
 
 import Foundation
 
-println("Hello, World!")
+//println("Hello, World!")
+
+
+var shoppingList = ["Eags", "Milk"]
+
+println("The shopping list contains \(shoppingList.count) items.")
+
+if shoppingList.isEmpty {
+    println("The shopping list is empty.")
+}else{
+    println("The shopping list is not empty.")
+}
+
+shoppingList.append("Flour")
+println("The shopping list contains \(shoppingList.count) items.")
+
+shoppingList += "Baking Power"
+println("The shopping list contains \(shoppingList.count) items.")
+
+shoppingList += ["Cheese", "Butter"]
+println("The shopping list contains \(shoppingList.count) items.")
 
