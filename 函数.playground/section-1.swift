365da8770e544b8526c5fba2a7191058d310c5e0
wwq0327 <wwq0327@gmail.com>
2014-09-27 11:59:11 +0800

    函数

--- 函数.playground/section-1.swift
+++ 函数.playground/section-1.swift
@@ -26,3 +26,43 @@ func sayHelloWorld() -> String {
 
 sayHelloWorld()
 
+// 无返回类型
+func sayGoodbye(personName: String) {
+    println("Goodbye, \(personName)!")
+}
+
+sayGoodbye("name")
+
+//多重返回值函数（Functions with Multiple Return Values)
+func count(string: String) -> (vowels: Int, consonants: Int, others: Int) {
+    var vowels = 0, consonants = 0, others = 0
+    for character in string {
+        switch String(character).lowercaseString {
+            case "a", "e", "i", "o", "u":
+            ++vowels
+        case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
+        "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z":
+            ++consonants
+        default:
+            ++others
+        }
+    }
+    return (vowels, consonants, others)
+}
+
+let total = count("my name is wyatt!")
+println(total)
+
+// 不定参数，多个返回值
+func duoReturn(nums:Int...) -> (sum: Int, avg: Int) {
+    var sum = 0, avg = 0
+    for num in nums {
+        sum += num
+    }
+    avg = sum / nums.count
+    return (sum, avg)
+}
+
+let d = duoReturn(1, 2, 3, 4, 5)
+
+
